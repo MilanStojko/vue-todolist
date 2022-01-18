312118ad7f14ff2c44add03910a432902a1ea517
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-01-18 13:14:23 +0100

    fixata generazione section

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let app = new Vue ({
         todo:[
             {
                 section: 'Stase',
+                sdone: false,
                 items: [
                     {
                         text: 'Discutere con Bernardini',
@@ -13,18 +14,42 @@ let app = new Vue ({
                     }
                 ]
             }
-        ],
+        ]
     },
     methods:{
-        addSection: function(){
-            this.todo.push({section:this.input, items:[]});
-        },
         addTask: function(){
-            this.todo.items[this.currentSection].push({text:this.input, done:false});
+            this.todo[this.currentSection].items.push({text:this.input, done:false});
             console.log()
-        }
-    }
-    
+        },
+        addSection: function(){
+            this.todo.push({section:this.input, sdone:false, items:[]});
+        },
+        deleteTask: function(i, y){
+            this.todo[i].items.splice(y, 1);
+        },
+        deleteSection: function(i){
+            this.todo.splice(i, 1);
+        },
+        fattaTask: function(i, y){
+            if(this.todo[y].items[i].done == false){
+                this.todo[y].items[i].done = true;
+            }
+        },
+        fattaSection: function(y){
+            if(this.todo[y].sdone == false){
+                this.todo[y].sdone = true;
+            }else{
+                this.todo[y].sdone = false;
+            }
+        },
+        fattaTask: function(y){
+            if(this.todo[y].items.done == false){
+                this.todo[y].items.done = true;
+            }else{
+                this.todo[y].items.done = false;
+            }
+        },
+    } 
 })
 
 console.log("mhanzzzz");
